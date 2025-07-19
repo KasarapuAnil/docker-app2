b87fa3795ba5019cc186d2e614a5900e5aac3cd5
KasarapuAnil <anil@xmachines.ai>
2025-07-19 17:59:42 +0530

    auto deploy

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React +xmachines</h1>
+      <h1>Vite + React +xmachines anil</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
